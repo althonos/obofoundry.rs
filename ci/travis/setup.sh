c6c0833be81ab38eccff85424662196ef8689532
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-03-19 23:24:10 +0100

    Add cargo-style color to `ci/travis/setup.sh` script

--- ci/travis/setup.sh
+++ ci/travis/setup.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
+log() {
+		tput bold
+		tput setaf 2
+		echo -n "$1" ""
+		tput sgr0
+		shift 1
+		echo $@
+}
+
+
 # --- Setup sscache ------------------------------------------------------------------
 LATEST=$(cargo search sccache | grep sccache | cut -f2 -d"\"")
 LOCAL=$(sccache --version 2>/dev/null | cut -f2 -d" " || echo "none")
 
 if [ "$LATEST" != "$LOCAL" ]; then
-	echo "Installing latest sccache v$LATEST (local: v$LOCAL)"
+	log "  Installing" "sccache v$LATEST"
 	URL="https://github.com/mozilla/sccache/releases/download/${LATEST}/sccache-${LATEST}-x86_64-unknown-linux-musl.tar.gz"
 	curl -SsL $URL | tar xzv -C /tmp
 	mv "/tmp/sccache-${LATEST}-x86_64-unknown-linux-musl/sccache" "$HOME/.cargo/bin/sccache"
 else
-	echo "Using cached sccache v$LOCAL"
+	log "       Using" "cached sccache v$LOCAL"
 fi
 
 
@@ -19,9 +29,9 @@ LATEST=$(cargo search cargo-tarpaulin | grep cargo-tarpaulin | cut -f2 -d"\"")
 LOCAL=$(cargo tarpaulin --version 2>/dev/null | cut -d" " -f3 || echo "none")
 
 if [ "$LATEST" != "$LOCAL" ]; then
-	echo "Downloading latest cargo-tarpaulin v$LATEST (local: v$LOCAL)"
+	log " Downloading" "cargo-tarpaulin v$LATEST"
 	URL="https://github.com/xd009642/tarpaulin/releases/download/${LATEST}/cargo-tarpaulin-${LATEST}-travis.tar.gz"
 	curl -SsL "$URL" | tar xzvC "$HOME/.cargo/bin"
 else
-	echo "Using cached cargo-tarpaulin v$LOCAL"
+	log "       Using" "cached cargo-tarpauline v$LOCAL"
 fi
