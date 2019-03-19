91b4243722941280b2bf93b9c9d1b2d1ccc76865
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-03-19 22:52:03 +0100

    Download prebuilt binaries when setting up Travis-CI build

--- ci/travis/setup.sh
+++ ci/travis/setup.sh
@@ -2,11 +2,13 @@
 
 # --- Setup sscache ------------------------------------------------------------------
 LATEST=$(cargo search sccache | grep sccache | cut -f2 -d"\"")
-LOCAL=$((sccache --version 2>/dev/null || echo "none") | cut -f2 -d" ")
+LOCAL=$(sccache --version 2>/dev/null | cut -f2 -d" " || echo "none")
 
 if [ "$LATEST" != "$LOCAL" ]; then
 	echo "Installing latest sccache v$LATEST (local: v$LOCAL)"
-	RUSTC_WRAPPER="" cargo install --path "$HOME/.local/bin" sccache
+	URL="https://github.com/mozilla/sccache/releases/download/${LATEST}/sccache-${LATEST}-x86_64-unknown-linux-musl.tar.gz"
+	curl -SsL $URL | tar xzv -C /tmp
+	mv "/tmp/sccache-${LATEST}-x86_64-unknown-linux-musl/sccache" "$HOME/.cargo/bin/sccache"
 else
 	echo "Using cached sccache v$LOCAL"
 fi
@@ -14,11 +16,12 @@ fi
 
 # --- Setup cargo-tarpaulin ----------------------------------------------------------
 LATEST=$(cargo search cargo-tarpaulin | grep cargo-tarpaulin | cut -f2 -d"\"")
-LOCAL=$((cargo tarpaulin --version 2>/dev/null || echo "none") | cut -d" " -f3)
+LOCAL=$(cargo tarpaulin --version 2>/dev/null | cut -d" " -f3 || echo "none")
 
 if [ "$LATEST" != "$LOCAL" ]; then
-	echo "Installing latest cargo-tarpaulin v$LATEST (local: v$LOCAL)"
-	cargo install --path "$HOME/.local/bin" cargo-tarpaulin
+	echo "Downloading latest cargo-tarpaulin v$LATEST (local: v$LOCAL)"
+	URL="https://github.com/xd009642/tarpaulin/releases/download/${LATEST}/cargo-tarpaulin-${LATEST}-travis.tar.gz"
+	curl -SsL "$URL" | tar xzvC "$HOME/.cargo/bin"
 else
 	echo "Using cached cargo-tarpaulin v$LOCAL"
 fi
