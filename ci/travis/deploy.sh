f2151e489adba8ea08bb9f89f25414608ec5074b
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-03-20 01:46:52 +0100

    Fix $PATH warning raised by Ruby in `ci/travis/deploy.sh`

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -17,11 +17,11 @@ cargo publish --token $CRATES_IO_TOKEN
 
 # --- Update release tags using Chandler ---------------------------------------
 
-log "  Installing" "chandler gem"
-gem install --user-install chandler
-
 export GEM_PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')"
 export PATH="${GEM_PATH}/bin:$PATH"
+
+log "  Installing" "chandler gem"
+gem install --user-install chandler
 
 log "    Updating" "GitHub release notes"
 chandler push --github="$TRAVIS_REPO_SLUG"
