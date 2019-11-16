60d0974f9ba9f7cc466ab6712dcc1758aa1efb32
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-11-16 15:19:53 +0100

    Allow test to succeed even when `cargo tarpaulin` fails

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -4,5 +4,10 @@
 
 # --- Test with coverage -----------------------------------------------------
 
-log Measuring code coverage with Tarpaulin
-cargo tarpaulin -v --out Xml --ciserver travis-ci
+if cargo tarpaulin -V >/dev/null 2>&1; then
+	log Measuring code coverage with Tarpaulin
+	cargo tarpaulin -v --out Xml --ciserver travis-ci
+else
+	log Testing code without coverage
+	cargo test
+fi
