b391e28535ca01957cc818d378b385c5e14c9e9d
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-04-10 11:47:13 +0200

    Streamline management of build tools in CI

--- ci/travis/setup.sh
+++ ci/travis/setup.sh
@@ -3,26 +3,16 @@
 . $(dirname $0)/functions.sh
 
 # --- Setup cargo-tarpaulin ----------------------------------------------------------
+
 LATEST=$(cargo search cargo-tarpaulin | grep cargo-tarpaulin | cut -f2 -d"\"")
-LOCAL=$(cargo tarpaulin --version 2>/dev/null | cut -d" " -f3 || echo "none")
+log Downloading cargo-tarpaulin v$LATEST
+URL="https://github.com/xd009642/tarpaulin/releases/download/${LATEST}/cargo-tarpaulin-${LATEST}-travis.tar.gz"
+curl -SsL "$URL" | tar xzvC "$HOME/.cargo/bin"
 
-if [ "$LATEST" != "$LOCAL" ]; then
-	log Downloading cargo-tarpaulin v$LATEST
-	URL="https://github.com/xd009642/tarpaulin/releases/download/${LATEST}/cargo-tarpaulin-${LATEST}-travis.tar.gz"
-	curl -SsL "$URL" | tar xzvC "$HOME/.cargo/bin"
-else
-	log Using cached cargo-tarpaulin v$LOCAL
-fi
-
+cargo tarpaulin --version
 
 # --- Setup cargo-cache ------------------------------------------------------
-LATEST=$(cargo search cargo-cache | head -n1 | cut -f2 -d"\"")
-LOCAL=$(cargo cache --version 2>/dev/null | cut -d" " -f2 || echo "none")
 
-if [ "$LATEST" != "$LOCAL" ]; then
-        log Installing cargo-cache v$LATEST
-        cargo install -f cargo-cache --root "$HOME/.cargo"
-else
-        log Using cached cargo-cache v$LOCAL
-fi
+log Installing latest cargo-cache
+cargo install -f cargo-cache --no-default-features --features ci-autoclean --root "$HOME/.cargo" 
 
