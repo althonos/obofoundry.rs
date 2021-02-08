8597753659fa274dc66be3dd63aade87b2fb6e5b
Martin Larralde <martin.larralde@embl.de>
2021-02-08 16:18:51 +0100

    Reformat `src/lib.rs` with `rustfmt`

--- src/lib.rs
+++ src/lib.rs
@@ -50,7 +50,9 @@
 //! and provides a [changelog](https://github.com/althonos/obofoundry.rs/blob/master/CHANGELOG.md)
 //! in the [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) format.
 
-#![doc(html_logo_url = "https://github.com/OBOFoundry/OBOFoundry.github.io/raw/master/images/foundrylogo.png")]
+#![doc(
+    html_logo_url = "https://github.com/OBOFoundry/OBOFoundry.github.io/raw/master/images/foundrylogo.png"
+)]
 
 extern crate serde;
 extern crate url;
@@ -107,7 +109,6 @@ fn mireots_vector<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
 where
     D: Deserializer<'de>,
 {
-
     #[derive(Deserialize, Debug)]
     #[serde(untagged)]
     pub enum Mireots {
